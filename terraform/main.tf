6a1eb2bb36ba7b340a1ea97503dc8519df8a5214
jishinzerogon <futago.r@icloud.com>
2026-04-09 10:18:22 +0900

    add s3 backend for terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "my-blog-tfstate-203369940478-ap-northeast-1-an"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {
@@ -15,3 +21,6 @@ provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
 }
+
+
+
