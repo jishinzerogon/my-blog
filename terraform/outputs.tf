66a688b51fd331033ef2759f57b7c5cc4763d5cb
jishinzerogon <futago.r@icloud.com>
2026-04-16 20:57:27 +0900

    add terraform outputs for cloudfront, alb, and ecr

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "cloudfront_domain_name" {
+  description = "CloudFront distribution domain name (set this as Cloudflare CNAME target)"
+  value       = aws_cloudfront_distribution.main.domain_name
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name (CloudFront origin)"
+  value       = aws_lb.main.dns_name
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL for Docker push"
+  value       = aws_ecr_repository.my_blog.repository_url
+}
