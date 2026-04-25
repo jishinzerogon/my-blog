1c70aee87781bf9947da5717e4f4a7ae2f2f0274
jishinzerogon <futago.r@icloud.com>
2026-04-25 22:42:35 +0900

    extend cloudfront custom error response cache ttl to 60s

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -26,14 +26,14 @@ resource "aws_cloudfront_distribution" "main" {
     error_code            = 403
     response_code         = 200
     response_page_path    = "/index.html"
-    error_caching_min_ttl = 10
+    error_caching_min_ttl = 60
   }
 
   custom_error_response {
     error_code            = 404
     response_code         = 200
     response_page_path    = "/index.html"
-    error_caching_min_ttl = 10
+    error_caching_min_ttl = 60
   }
 
   restrictions {
